76cc3f3bcc89a3712c90d73fd6f868965a9033e8
Christian Gerber <cgerber3@illinois.edu>
2022-07-28 13:13:55 -0400

    More updates

--- js/election_force_directed_results.js
+++ js/election_force_directed_results.js
@@ -1,28 +1,37 @@
-var USER_SPEED = "fast";
-
 var width = 750,
-    height = 750,
+    height = 450,
 	padding = 1,
-	maxRadius = 3;
-	// color = d3.scale.category10();
+	maxRadius = 6;
 
 var sched_objs = [],
 	curr_slide = 0;
 
 var act_codes = [
-	{"index": "0", "short": "Republican", "desc": "Republican"},
-	{"index": "1", "short": "Democrat", "desc": "Democrat"},
+	{"index": "0", "short": "Democrat", "desc": "Democrat"},
+	{"index": "1", "short": "Republican", "desc": "Republican"},
 	{"index": "2", "short": "Independent", "desc": "Independent"}
 ];
 
-
-var speeds = { "slow": 200, "medium": 125, "fast": 75 };
-
-
-var notes_index = 0;
+var notes = [
+	{"year": "1964", "demCanImg": "img/Lyndon_Johnson.jpg", "demCanName": "Lyndon B. Johnson", "repCanImg": "img/Barry_Goldwater.jpg", "repCanName": "Barry Goldwater", "note": "The 1964 election marked the beginning of a major, long-term re-alignment in American politics, as Goldwater's unsuccessful bid significantly influenced the modern conservative movement. The movement of conservatives to the Republican Party continued, culminating in the 1980 presidential victory of Ronald Reagan."},
+	{"year": "1968", "demCanImg": "img/Hubert_Humphrey.jpg", "demCanName": "Hubert Humphrey", "repCanImg": "img/Richard_Nixon.jpg", "repCanName": "Richard Nixon", "note": ""},
+	{"year": "1972", "demCanImg": "img/George_McGovern.jpg", "demCanName": "George McGovern", "repCanImg": "img/Richard_Nixon.jpg", "repCanName": "Richard Nixon", "note": ""},
+	{"year": "1976", "demCanImg": "img/Jimmy_Carter.jpg", "demCanName": "Jimmy Carter", "repCanImg": "img/Gerald_Ford.jpg", "repCanName": "Gerald Ford", "note": ""},
+	{"year": "1980", "demCanImg": "img/Jimmy_Carter.jpg", "demCanName": "Jimmy Carter", "repCanImg": "img/Ronald_Reagan.jpg", "repCanName": "Ronald Reagan", "note": ""},
+	{"year": "1984", "demCanImg": "img/Walter_Mondale.jpg", "demCanName": "Walter Mondale", "repCanImg": "img/Ronald_Reagan.jpg", "repCanName": "Ronald Reagan", "note": ""},
+	{"year": "1988", "demCanImg": "img/Michael_Dukakis.jpg", "demCanName": "Micheal Dukakis", "repCanImg": "img/George_H_W_Bush.jpg", "repCanName": "George H. W. Bush", "note": ""},
+	{"year": "1992", "demCanImg": "img/Bill_Clinton.jpg", "demCanName": "Bill Clinton", "repCanImg": "img/George_H_W_Bush.jpg", "repCanName": "George H. W. Bush", "note": ""},
+	{"year": "1996", "demCanImg": "img/Bill_Clinton.jpg", "demCanName": "Bill Clinton", "repCanImg": "img/Bob_Dole.jpg", "repCanName": "Bob Dole", "note": ""},
+	{"year": "2000", "demCanImg": "img/Al_Gore.jpg", "demCanName": "Al Gore", "repCanImg": "img/George_W_Bush.jpg", "repCanName": "George W. Bush", "note": ""},
+	{"year": "2004", "demCanImg": "img/John_Kerry.jpg", "demCanName": "John Kerry", "repCanImg": "img/George_W_Bush.jpg", "repCanName": "George W. Bush", "note": ""},
+	{"year": "2008", "demCanImg": "img/Barack_Obama.jpg", "demCanName": "Barack Obama", "repCanImg": "img/John_McCain.jpg", "repCanName": "John McCain", "note": ""},
+	{"year": "2012", "demCanImg": "img/Barack_Obama.jpg", "demCanName": "Barack Obama", "repCanImg": "img/Mitt_Romney.jpg", "repCanName": "Mitt Romney", "note": ""},
+	{"year": "2016", "demCanImg": "img/Hillary_Clinton.jpg", "demCanName": "Hillary Clinton", "repCanImg": "img/Donald_Trump.jpg", "repCanName": "Donald Trump", "note": ""},
+	{"year": "2020", "demCanImg": "img/Joe_Biden.jpg", "demCanName": "Joe Biden", "repCanImg": "img/Donald_Trump.jpg", "repCanName": "Donald Trump", "note": ""},
+]
 
 var center_act = "Independent",
-	center_pt = { "x": 380, "y": 365 };
+	center_pt = { "x": 380, "y": 225 };
 
 // Coordinates for activities
 var foci = {};
@@ -31,7 +40,7 @@ act_codes.forEach(function(code, i) {
 		foci[code.index] = center_pt;
 	} else {
 		var theta = 2 * Math.PI / (act_codes.length-1);
-		foci[code.index] = {x: 250 * Math.cos((i - 1) * theta)+380, y: 250 * Math.sin((i - 1) * theta)+365 };
+		foci[code.index] = {x: 250 * Math.cos((i - 1) * theta)+380, y: 250 * Math.sin((i - 1) * theta)+225 };
 	}
 });
 
@@ -59,16 +68,16 @@ d3.csv("data/presdential_results.csv", function(data) {
 		for(var j = 0; j < 538; j++) {
 			if(republican > 0) {
 				if(i == 0) {
-					sched_objs.push([0])
+					sched_objs.push([1])
 				} else {
-					sched_objs[j].push(0);
+					sched_objs[j].push(1);
 				}
 				republican -= 1;
 			} else if(democrat > 0) {
 				if(i == 0) {
-					sched_objs.push([1])
+					sched_objs.push([0])
 				} else {
-					sched_objs[j].push(1);
+					sched_objs[j].push(0);
 				}
 				democrat -= 1;
 			} else if(independent > 0) {
@@ -105,7 +114,6 @@ d3.csv("data/presdential_results.csv", function(data) {
 	force = d3.layout.force()
 		.nodes(nodes)
 		.size([width, height])
-		// .links([])
 		.gravity(0)
 		.charge(0)
 		.friction(.9)
@@ -136,7 +144,7 @@ d3.csv("data/presdential_results.csv", function(data) {
 				return center_pt.y;
 			} else {
 				var theta = 2 * Math.PI / (act_codes.length-1);
-				return 340 * Math.sin((i - 1) * theta)+365;
+				return 340 * Math.sin((i - 1) * theta)+225;
 			}
 		});
 
@@ -153,10 +161,77 @@ d3.csv("data/presdential_results.csv", function(data) {
 		.attr("text-anchor", "middle")
 		.attr("class", "actpct")
 		.text(function(d) {
-			return act_counts[d.index] + "%";
+			return readablePercent(act_counts[d.index]);
 		});
+	
+	d3.select("#year")
+		.style("color", "#fffced")
+		.style("text-align", "left")
+		.style("font-size", "200%")
+		.style("font-family", "adobe-caslon-pro")
+		.text(notes[curr_slide].year)
+		.transition()
+		.duration(500)
+		.style("text-align", "center")
+		.style("color", "#000000");
 
-	d3.selectAll(".nextbutton").on("click", function() {
+	d3.select("#demImg")
+		.attr('src', notes[curr_slide].demCanImg)
+		.attr('width', 200)
+		.attr('height', 250)
+		.style('position', 'relative')
+		.style('opacity', 0)
+		.style('margin-right', '325px')
+		.style("display", "block")
+		.style("background", "blue")
+		.style("padding", "8px")
+		.style("border", "1px solid #ccc")
+		.style("box-shadow", "5px 5px 5px #999")
+		.transition()
+		.duration(1000)
+		.style('opacity', 1);
+
+	d3.select("#repImg")
+		.attr('src', notes[curr_slide].repCanImg)
+		.attr('width', 200)
+		.attr('height', 250)
+		.style('position', 'relative')
+		.style('opacity', 0)
+		.style("display", "block")
+		.style("background", "red")
+		.style("padding", "8px")
+		.style("border", "1px solid #ccc")
+		.style("box-shadow", "5px 5px 5px #999")
+		.transition()
+		.duration(1000)
+		.style('opacity', 1);
+
+	d3.select("#demName")
+		.style("color", "#fffced")
+		.style("text-align", "left")
+		.style("font-size", "150%")
+		.style("font-family", "adobe-caslon-pro")
+		.style("top", "10px")
+		.style('margin-right', '375px')
+		.text(notes[curr_slide].demCanName)
+		.transition()
+		.duration(500)
+		.style("text-align", "center")
+		.style("color", "#000000");
+	
+	d3.select("#repName")
+		.style("color", "#fffced")
+		.style("text-align", "left")
+		.style("font-size", "150%")
+		.style("font-family", "adobe-caslon-pro")
+		.style("top", "10px")
+		.text(notes[curr_slide].repCanName)
+		.transition()
+		.duration(500)
+		.style("text-align", "center")
+		.style("color", "#000000");
+
+	d3.select(".nextbutton").on("click", function() {
 		if(curr_slide < data.length) {
 			curr_slide += 1;
 			d3.range(nodes.length).map(function(i) {
@@ -174,12 +249,67 @@ d3.csv("data/presdential_results.csv", function(data) {
 			.text(function(d) {
 				return readablePercent(act_counts[d.index]);
 			});
+			
+			//Update Year
+			d3.select("#year")
+				.text(notes[curr_slide].year)
+				.transition()
+				.duration(500)
+				.style("text-align", "center")
+				.style("color", "#000000");
+
+			d3.select("#demImg")
+				.attr('src', notes[curr_slide].demCanImg)
+				.attr('width', 200)
+				.attr('height', 250)
+				.style('position', 'relative')
+				.style('opacity', 0)
+				.style('margin-right', '325px')
+				.style("display", "block")
+				.style("background", "blue")
+				.style("padding", "8px")
+				.style("border", "1px solid #ccc")
+				.style("box-shadow", "5px 5px 5px #999")
+				.transition()
+				.duration(1000)
+				.style('opacity', 1);
+
+			d3.select("#repImg")
+				.attr('src', notes[curr_slide].repCanImg)
+				.attr('width', 200)
+				.attr('height', 250)
+				.style('position', 'relative')
+				.style('opacity', 0)
+				.style("display", "block")
+				.style("background", "red")
+				.style("padding", "8px")
+				.style("border", "1px solid #ccc")
+				.style("box-shadow", "5px 5px 5px #999")
+				.transition()
+				.duration(1000)
+				.style('opacity', 1);
+			
+			// Update names
+
+			d3.select("#demName")
+				.text(notes[curr_slide].demCanName)
+				.transition()
+				.duration(500)
+				.style("text-align", "center")
+				.style("color", "#000000");
+			
+			d3.select("#repName")
+				.text(notes[curr_slide].repCanName)
+				.transition()
+				.duration(500)
+				.style("text-align", "center")
+				.style("color", "#000000");
 		}
 	});
 
-	d3.selectAll(".prevbutton").on("click", function() {
-		if(curr_slide < data.length) {
-			curr_slide += 1;
+	d3.select(".prevbutton").on("click", function() {
+		if(curr_slide > 0) {
+			curr_slide -= 1;
 			d3.range(nodes.length).map(function(i) {
 				var current_node = nodes[i];
 				act_counts[current_node.act] -= 1;
@@ -195,6 +325,60 @@ d3.csv("data/presdential_results.csv", function(data) {
 			.text(function(d) {
 				return readablePercent(act_counts[d.index]);
 			});
+
+			//Update Year
+			d3.select("#year")
+				.text(notes[curr_slide].year)
+				.transition()
+				.duration(500)
+				.style("text-align", "center")
+				.style("color", "#000000");
+
+			d3.select("#demImg")
+				.attr('src', notes[curr_slide].demCanImg)
+				.attr('width', 200)
+				.attr('height', 250)
+				.style('position', 'relative')
+				.style('opacity', 0)
+				.style('margin-right', '325px')
+				.style("display", "block")
+				.style("background", "blue")
+				.style("padding", "8px")
+				.style("border", "1px solid #ccc")
+				.style("box-shadow", "5px 5px 5px #999")
+				.transition()
+				.duration(1000)
+				.style('opacity', 1);
+	
+			d3.select("#repImg")
+				.attr('src', notes[curr_slide].repCanImg)
+				.attr('width', 200)
+				.attr('height', 250)
+				.style('position', 'relative')
+				.style('opacity', 0)
+				.style("display", "block")
+				.style("background", "red")
+				.style("padding", "8px")
+				.style("border", "1px solid #ccc")
+				.style("box-shadow", "5px 5px 5px #999")
+				.transition()
+				.duration(1000)
+				.style('opacity', 1);
+
+			// Update names
+			d3.select("#demName")
+				.text(notes[curr_slide].demCanName)
+				.transition()
+				.duration(500)
+				.style("text-align", "center")
+				.style("color", "#000000");
+			
+			d3.select("#repName")
+				.text(notes[curr_slide].repCanName)
+				.transition()
+				.duration(500)
+				.style("text-align", "center")
+				.style("color", "#000000");
 		}
 	})
 
@@ -252,8 +436,8 @@ d3.csv("data/presdential_results.csv", function(data) {
 function color(activity) {
 
 	var colorByActivity = {
-		"0": "blue",
 		"1": "red",
+		"0": "blue",
 		"2": "yellow"
 	}
 
@@ -265,8 +449,8 @@ function color(activity) {
 // Output readable percent based on count.
 function readablePercent(n) {
 
-	var pct = 100 * n / 1000
-	;
+	var pct = n/538 * 100;
+
 	if (pct < 1 && pct > 0) {
 		pct = "<1%";
 	} else {
